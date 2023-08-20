169daf4144c6efedd17cb6a4e03103738998daab
valentino <v.arballo@itecriocuarto.org.ar>
2023-08-20 19:53:54 -0300

    todo listo

---
+++ src/components/reloj.jsx
@@ -0,0 +1,36 @@
+import React, {Component} from "react";
+
+class Clock extends Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+          currentTime: new Date()
+        };
+      }
+    
+      componentDidMount() {
+        this.intervalID = setInterval(() => {
+          this.setState({
+            currentTime: new Date()
+          });
+        }, 1000);
+      }
+    
+      componentWillUnmount() {
+        clearInterval(this.intervalID);
+      }
+
+    render () {
+        var tiempoActual = new Date();
+        var horas = tiempoActual.getHours();
+        var minutos = tiempoActual.getMinutes();
+        var segundos = tiempoActual.getSeconds();
+
+        return (
+            <h1>{horas}:{minutos}:{segundos}</h1>
+        );
+    }
+}
+
+export default Clock;
