6adfdd024c1c40870d067a44e0a595095223453f
valentino <v.arballo@itecriocuarto.org.ar>
2023-08-31 14:21:21 -0300

    conditional rendering + useState

--- src/App.js
+++ src/App.js
@@ -5,11 +5,14 @@ import Saludar from './components/saludar';
 import Toast from './components/toast';
 import MostrarImagen from './components/imagen';
 import Personas from './components/personas';
+import { Test } from './components/test';
+import { useState } from 'react';
 
 function App() {
-
-  const listaNombres = ["Pedro Lucero", "Lionel Messi","Valentino Arballo", "Carlitos Tevez"];
-  const listaEdades = [5, 35,20];
+  const [isToggled, setIsToggled] = useState(false);
+  const listaNombres = ["Pedro Lucero", "Lionel Messi"];
+  const listaEdades = [5, 35];
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -25,6 +28,9 @@ function App() {
         <MostrarImagen />
 
         <Personas listaNombres={listaNombres} listaEdades = {listaEdades}/>
+        <button onClick={() => setIsToggled(!isToggled)}>toggle</button>
+        {isToggled && <Test />}
+        {isToggled ? <Test /> : <p>not toggled</p>}
 
       </header>
     </div>
---
+++ src/components/test.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+
+export const Test = () => {
+  return(
+    <div>
+      <p>conditional rendering</p>
+    </div>
+  )
+}
