ab34a2ed7411dc1e3fbd68eb4806d99a4f822c5e
valentino <v.arballo@itecriocuarto.org.ar>
2023-09-04 19:44:38 -0300

    TEST

--- src/components/toast.jsx
+++ src/components/toast.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 
 const mensaje = () => {
-    toast.success('Soy un toast', {
+    toast.danger('Soy un toast', {
       position: "top-right",
-      autoClose: 5000,
+      autoClose: 2500,
       hideProgressBar: false,
       closeOnClick: true,
       pauseOnHover: true,
